4141fc6a74ef83b3c0e283f0c8b3e2faf2508fef
aubrxyy <aubreymantiri@gmail.com>
2024-11-11 21:41:05 +0700

    layout revamp

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const interR = Inter({
   subsets: ['latin'],
@@ -23,9 +24,9 @@ export function Hero() {
                 <p className={`${interB.className}`}>tail</p>
               </div>
               <div className="w-full lg:w-[60ch] mb-4">Cupang crown tail menjadi mendunia karena variasi keindahannya. Disebut crown tail atau ekor mahkota, karena bila dibalik menghadap ke atas serit-serit pada ekornya terlihat seperti mahkota raja.</div>
-              <button className="border-[1px] w-fit px-8 py-3 rounded-md border-white mx-auto lg:mx-0 button-slide">
+              <Link href="/catalog" className="border-[1px] w-fit px-8 py-3 rounded-md border-white mx-auto lg:mx-0 button-slide">
                 <span>Shop now</span>
-              </button>
+              </Link>
               </div>
             <div className="flex justify-center">
               <Image src="/herofish1.png" alt="Ikan" width={600} height={600} className="w-full max-w-xs lg:max-w-none lg:h-[35rem] lg:w-[35rem]"/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
-'use client'
-
-import { useRouter } from 'next/navigation';
 import { ArticleHome } from './_components/ArticleHome';
 import { Hero } from './_components/Hero';
 import { NewArrival } from './_components/NewArrival';
-import { setCookie } from './_utils/cookies';
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    const exp = new Date(0); 
-    setCookie('currentUser', '', { expires: exp, secure: true, sameSite: 'Strict' });
-    setCookie('userRole', '', { expires: exp, secure: true, sameSite: 'Strict' });
-    setCookie('userId', '', { expires: exp, secure: true, sameSite: 'Strict' });
-    setCookie('username', '', { expires: exp, secure: true, sameSite: 'Strict' });
-    localStorage.removeItem('token');
-    router.push('/login');
-  };
 
   return (
     <>
